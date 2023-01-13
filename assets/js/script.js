42f731b417fae920513d973f16b192c5b9c4bb91
Kristen Schaefer <kristencschaefer@gmail.com>
2023-01-12 18:09:14 -0600

    updated layout and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,9 +5,6 @@ $(function () {
   const dateEl = $("#currentDate");
   const today = moment().format("MMMM Do, YYYY");
   dateEl[0].innerHTML = today;
-  console.log(dateEl);
-
-
 
   // search for multpile cities, each city will populate a button under the search bar to link back to that city data (two entry points into the same API)
 
@@ -20,3 +17,8 @@ $(function () {
   // get icons here http://openweathermap.org/img/wn/$%7Bicon%7D@2x.png
   // or here https://openweathermap.org/weather-conditions
 });
+
+//will have to do 2 api calls (one for lat/long from city, next for 5 day forecast)
+
+// use postman.com to help with creating the api url + test
+
