b60be1d84048b9bc002424d6781b86b370b2f4fd
Kristen Schaefer <kristencschaefer@gmail.com>
2023-01-12 20:45:47 -0600

    added local storage setting

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,36 @@
 
 $(function () {
+  
+  let locations = JSON.parse(localStorage.getItem('locations'));
 
+  if (!locations) { // checks to see if there is anything in local storage first
+    locations = []; // creates an object for local strorage if none exists
+  };
 
   const dateEl = $("#currentDate");
   const today = moment().format("MMMM Do, YYYY");
   dateEl[0].innerHTML = today;
+
+  const locationEl = $("#locationSearch");
+  const submitBTN = $("#locationSubmit");
+  
+  
+  submitBTN.click(function (event) {
+    //create array for local storage
+    locations.push(locationEl.val());
+    localStorage.setItem('locations', JSON.stringify(locations));
+    
+    //make sure this pushes to the array
+  });
+
+
+  // saveBtnEl.click(function (event) {
+  //   const inputText = (event.currentTarget.parentElement.children[1].value.trim()); // user input text
+  //   const inputTime = (event.currentTarget.parentElement.id); // grabs the time block for user input
+  //   calEvents[inputTime] = inputText; // ties the time block to the input text
+  //   localStorage.setItem('calEvents', JSON.stringify(calEvents)); // sets it to local storage
+  // })
+
 
   // search for multpile cities, each city will populate a button under the search bar to link back to that city data (two entry points into the same API)
 
