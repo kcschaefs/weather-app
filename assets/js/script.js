fbb1ad0274f7961564d1dddc02aef422afc0b355
Kristen Schaefer <kristencschaefer@gmail.com>
2023-01-14 13:58:26 -0600

    added api and temp conversion functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,10 @@
 
 $(function () {
 
+  // search and buttons -------------------------------------------------
+
+  // search for multpile cities, each city will populate a button under the search bar to link back to that city data (two entry points into the same API)
+
   let locations = JSON.parse(localStorage.getItem('locations'));
 
   if (!locations) { // checks to see if there is anything in local storage first
@@ -8,8 +12,8 @@ $(function () {
   } else {
     createBtn();
   };
-  
-  const dateEl = $("#currentDate");
+
+  const dateEl = $("#currentDate"); // shows current date on page
   const today = moment().format("MMMM Do, YYYY");
   dateEl[0].innerHTML = today;
 
@@ -19,50 +23,93 @@ $(function () {
 
   function removeBtn() {
     var grabBtn = $("#storedLocation");
-        grabBtn.children().remove();
-      }
+    grabBtn.children().remove();
+  }
 
-  function createBtn() {
+  function createBtn() { // removes the current buttons from the array so the new set of buttons can be added
     removeBtn();
     for (var city of locations) {
-      //need to only add new buttons, not return the full array on each click
       buttonEl = $('<input/>').attr({
         type: "button",
-        id: "savedLocation",
+        class: "locationBtn",
+        id: `savedLocation-${city}`,
         value: city,
       });
       $("#storedLocation").append(buttonEl);
     }
   };
 
-  submitBtn.click(function (event) {
+  // city names need to be stored in local data
+  submitBtn.click(function (event) { // adds all the items from local storage into the saved searches buttons
     if (locationEl.val() == "") {
       return;
     } else {
-      // removeBtn(); // won't allow for more city inputs
       locations.push(locationEl.val());
       localStorage.setItem('locations', JSON.stringify(locations));
-      // don't want this tied to button click only, add to document and allow for a call here?
-      //set this to only serve the newest city on click 
       createBtn();
-      //this is not working
       locationEl.val("");
     }
   });
-  
 
-  // saveBtnEl.click(function (event) {
-  //   const inputText = (event.currentTarget.parentElement.children[1].value.trim()); // user input text
-  //   const inputTime = (event.currentTarget.parentElement.id); // grabs the time block for user input
-  //   calEvents[inputTime] = inputText; // ties the time block to the input text
-  //   localStorage.setItem('calEvents', JSON.stringify(calEvents)); // sets it to local storage
-  // })
+  // api calls --------------------------------------------------------
 
+  function getApiLocation() { // this gets the coordinates for the user input city
+    var requestUrl = "http://api.openweathermap.org/geo/1.0/direct?q=Minneapolis&limit=1&appid=66b175c500dd9fc7665e0ac4fc3cff12";
 
-  // search for multpile cities, each city will populate a button under the search bar to link back to that city data (two entry points into the same API)
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        // console.log(data[0].lat);
+        // console.log(data[0].lon);
+      })
+  };
 
+  getApiLocation();
 
-  // city names need to be stored in local data
+  function getApiWeather() { // this gets the forecast for the coordinates pulled from the first api
+    var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?lat=44.9772995&lon=-93.2654692&appid=66b175c500dd9fc7665e0ac4fc3cff12";
+
+    fetch(requestUrl)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        parseWeatherData(data);
+      })
+  };
+
+  //this code was provided by professor to pull out the specific time sets from the provided data
+  let currDTValue = "";
+  const fiveDaysOfWeather = []
+
+  function parseWeatherData(data) {
+    data.list.forEach(obj => {
+      const dateObj = new moment.unix(obj.dt)
+      // console.log(dateObj.date());
+      const currDay = dateObj.date();
+
+      if (currDay !== currDTValue && fiveDaysOfWeather.length < 6 && !fiveDaysOfWeather.find(day => day.dt === obj.dt)) {
+        currDTValue = currDay
+        fiveDaysOfWeather.push(obj)
+        tempChange(obj);  // added this in so I can loop through the temperature conversion without having to create a new loop
+      }
+    })
+  };
+  // end of provided code
+
+  getApiWeather();
+
+  function tempChange(obj) { // this converts the temp from the provided kelvin to faranheit
+    let kelvin = obj.main.temp; //grabs the temp out of the populated array
+    let farenh = (kelvin - 273.15) * 9 / 5 + 32; //converts to farenheit
+    obj.main.tempf = Math.round(farenh); // rounds to the nearest whole number because I don't care about decimal places in a temperature, I want the whole number
+  };
+
+  // weather info serving -----------------------------------------------------------
+
+  // console.log("t");
 
   // will need to convert temp from kelvin to faranheit
 
